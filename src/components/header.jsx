bef724db43f57137b26fedbdf92a003b7ecd4825
David Goldberg <drgold1988@gmail.com>
2015-08-22 14:01:40 -0700

    [feat]: renders first four topics to the header with param routes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,22 @@
 var React = require('react');
 var Router = require('react-router');
 var Link = Router.Link;
+var Reflux = require('reflux');
+var TopicActions = require('../actions/topic-actions');
+var TopicStore = require('../stores/topic-store');
 
 var Header = React.createClass({
+  mixins: [
+    Reflux.listenTo(TopicStore, 'onChange')
+  ],
+  getInitialState: function() {
+    return {
+      topics: []
+    }
+  },
+  componentWillMount: function() {
+    TopicActions.getTopics();
+  },
   render: function() {
     return (
       <nav className="navbar navbar-default header">
@@ -11,11 +25,27 @@ var Header = React.createClass({
             Image Client
           </Link>
           <ul className="nav navbar-nav navbar-right">
-            <li><a>Topic #1</a></li>
+            {this.renderTopics()}
           </ul>
         </div>
       </nav>
     );
+  },
+  renderTopics: function() {
+    return this.state.topics.slice(0,4).map(function(topic) {
+      return (
+        <li key={topic.id}>
+          <Link to={"topics/" + topic.id}>
+            {topic.name}
+          </Link>
+        </li>
+      );
+    });
+  },
+  onChange: function(event, topics) {
+    this.setState({
+      topics: topics
+    });
   }
 });
 
