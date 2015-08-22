19650ecedb30bea042e5377e8efbc09e1ce27b25
David Goldberg <drgold1988@gmail.com>
2015-08-22 01:46:16 -0700

    [feat]: uses content func to determine display content

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,16 +1,23 @@
 var React = require('react');
 var Header = require('./header');
+var TopicList = require('./topic-list');
 
 var Main = React.createClass({
   render: function() {
     return (
       <div>
         <Header />
-        {this.props.children}
+        {this.content()}
       </div>
     );
+  },
+  content: function() {
+    if (this.props.children) {
+      return this.props.children
+    } else {
+      return <TopicList />
+    }
   }
-
 });
 
 module.exports = Main;
