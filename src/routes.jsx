4fe0814c434b8b234df2f4d037b8401bf41f7093
David Goldberg <drgold1988@gmail.com>
2015-08-22 14:02:54 -0700

    [feat]: adds routing for nested topics with params for topic id

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,10 +5,13 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
 var Main = require('./components/main');
+var Topic = require('./components/topic');
 
 var routes = (
   <Router history={new HashHistory}>
-    <Route path="/" component={Main} />
+    <Route path="/" component={Main}>
+      <Route path="topics/:id" component={Topic} />
+    </Route>
   </Router>
 );  
 
