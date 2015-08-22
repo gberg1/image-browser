7cae5653a0f4362e31ebbba471b9f7cdb8361814
David Goldberg <drgold1988@gmail.com>
2015-08-22 13:18:29 -0700

    [feat]: creates event listener and topics data rendering for reflux

--- src/components/topic-list.jsx
+++ src/components/topic-list.jsx
@@ -1,12 +1,39 @@
 var React = require('react');
+var Reflux = require('reflux');
+var TopicStore = require('../stores/topic-store');
+var TopicActions = require('../actions/topic-actions');
 
 var TopicList = React.createClass({
+  mixins: [
+    Reflux.listenTo(TopicStore, 'onChange')
+  ],
+  getInitialState: function() {
+    return {
+      topics: []
+    }
+  },
+  componentWillMount: function() {
+    TopicActions.getTopics();
+  },
   render: function() {
     return (
       <div className="list-group">
         Topic List
+        {this.renderTopics()}
       </div>
     );
+  },
+  renderTopics: function() {
+    return this.state.topics.map(function(topic) {
+      return (
+        <li>
+          {topic}
+        </li>
+      );
+    });
+  },
+  onChange: function(event, topics) {
+    this.setState({topics: topics});
   }
 });
 
