659fe24716e1e232297997e176872f56204820ca
David Goldberg <drgold1988@gmail.com>
2015-08-22 01:17:05 -0700

    [feat]: makes get request to imgur api using fetch

---
+++ src/utils/api.jsx
@@ -0,0 +1,14 @@
+var Fetch = require('whatwg-fetch');
+var rootUrl = 'https://api.imgur.com/3/';
+var keys = require('./config');
+var apiKey = keys.apiKey;
+
+module.exports = {
+  get: function(url) {
+    return fetch(rootUrl + url, {
+      headers: {
+        'Authorization': 'Client-ID ' + apiKey
+      }
+    });
+  }
+}
