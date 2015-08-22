107170f13257c283be8eff07c7b63b173a73a4a0
David Goldberg <drgold1988@gmail.com>
2015-08-22 01:46:42 -0700

    [feat]: creates TopicList component

---
+++ src/components/topic-list.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var TopicList = React.createClass({
+  render: function() {
+    return (
+      <div className="list-group">
+        Topic List
+      </div>
+    );
+  }
+});
+
+module.exports = TopicList;
+
