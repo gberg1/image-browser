5b8c258d8d63f2a42db54526548604f8a8fe2202
David Goldberg <drgold1988@gmail.com>
2015-08-22 17:13:05 -0700

    [style]: adds image-preview class to main div in render

--- src/components/image-preview.jsx
+++ src/components/image-preview.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var ImagePreview = React.createClass({
   render: function() {
     return (
-      <div>
+      <div className="image-preview">
         {this.image()}
       </div>
     );
